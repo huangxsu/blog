f5334c860d704126931aad7d095b9f39a8d382c1
suj <suj@101.com>
2018-05-10 19:04:35 +0800

    https

--- themes/nayo/scripts/render_img.js
+++ themes/nayo/scripts/render_img.js
@@ -15,28 +15,31 @@ function renderImg(source) {
         let origin = $(element).attr('data-origin');
 
         if (origin && origin !== placeHolder) {
-           
-                $(element).attr({
-                    'data-src': origin
-                });
-                $(element).css('background-image', `url("${placeHolder}")`);
-                $(element).addClass('lazyload');
-           
+
+            $(element).attr({
+                'data-src': origin
+            });
+            $(element).css('background-image', `url("${placeHolder}")`);
+            $(element).addClass('lazyload');
+
         }
     });
 
     img.each((idx, element) => {
-
-        let src = $(element).attr('src');
+        let el = $(element);
+        if (el.hasClass('donate-img')) {
+            return true;
+        }
+        let src = el.attr('src');
 
         if (src && src !== placeHolder) {
-            $(element).attr({
+            el.attr({
                 'data-src': src,
                 'src': placeHolder
             });
         }
 
-        $(element).addClass('lazyload');
+        el.addClass('lazyload');
     });
 
     return $.html()
