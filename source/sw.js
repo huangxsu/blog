258e8659954a1a01174ca0f4e53c9ad1fc233356
sujun <sujun01@corp.netease.com>
2019-04-24 16:47:18 +0800

    cache and network

--- source/sw.js
+++ source/sw.js
@@ -7,11 +7,7 @@ var filesToCache = [
 ]
 
 self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache)
-    })
-  )
+  event.waitUntil(precache())
 })
 
 self.addEventListener('activate', function (event) {
@@ -26,10 +22,31 @@ self.addEventListener('activate', function (event) {
   )
 })
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request)
-    })
-  )
+self.addEventListener('fetch', function (evt) {
+  evt.respondWith(fromCache(evt.request))
+  if(filesToCache.indexOf(evt.request.url) !== -1){
+    evt.waitUntil(update(evt.request))
+  }
 })
+
+function precache(){
+  return caches.open(cacheName).then(function (cache) {
+    return cache.addAll(filesToCache)
+  })
+}
+
+function fromCache(request){
+  return caches.open(cacheName).then(function(cache){
+    return cache.match(request).then(function(matching){
+      return matching || fetch(request)
+    })
+  })
+}
+
+function update(request){
+  return caches.open(cacheName).then(function(cache){
+    return fetch(request).then(function(response){
+      return cache.put(request,response)
+    })
+  })
+}
